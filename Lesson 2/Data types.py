b443f9bea619f514e4e6aa838aaa8d1eed8ebbc6
Grace Manoj <helvimanoj@gmail.com>
2025-11-08 15:52:08 +0300

    commit

---
+++ Lesson 2/Data types.py
@@ -0,0 +1,19 @@
+name="Grace"
+age=15
+student=True
+weight=46.5
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :",age)
+print("Data Type of Age is", type(age))
+print("student :",student)
+print("Data Type of Student is", type(student))
+print("Weight :",weight)
+print("Data Type of Weight is", type(weight))
+print("\n After Type Casting....")
+age=str(age)
+print(age)
+print("Data type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
