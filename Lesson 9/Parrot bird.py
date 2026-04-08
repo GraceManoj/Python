16b1497ec10de6599dc571404adf9280647c664a
Grace Manoj <helvimanoj@gmail.com>
2026-04-08 15:43:43 +0300

    commit

---
+++ Lesson 9/Parrot bird.py
@@ -0,0 +1,21 @@
+class Parrot:
+
+    #class attribute
+    species="bird"
+
+    #instance variable
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+#instantiate the parrot class
+blu=Parrot("Blu",10)
+woo=Parrot("Woo",12)
+
+#access the class attributes
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+
+#accesss the instance attributes
+print("{} is {} years old".format(blu.name,blu.age))
+print("{} is {} years old".format(woo.name,woo.age))
