b7e29b9e9bcdeb1f4b7443e28ee0383529e268e9
Grace Manoj <helvimanoj@gmail.com>
2026-04-15 15:46:55 +0300

    commit

---
+++ Lessson 10/Constructor and Destructor.py
@@ -0,0 +1,10 @@
+class Employee:
+    #Initialising(constructor)
+    def __init__(self):
+        print('Employee created.')
+    
+    #Deleting(Destructor)
+    def __del__(self):
+        print('Destructor called, Employee deleted')
+obj=Employee()
+del obj
